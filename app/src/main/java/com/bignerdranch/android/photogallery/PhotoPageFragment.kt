5144935d8b85142962a85e1e8f5f6406ce3e6fcd
cheng <chengl@bu.edu>
2024-03-28 23:23:08 -0400

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
@@ -0,0 +1,97 @@
+package com.bignerdranch.android.photogallery
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bignerdranch.android.photogallery.databinding.FragmentPhotoPageBinding
+
+class PhotoPageFragment : Fragment() {
+    private val args: PhotoPageFragmentArgs by navArgs()
+    private lateinit var binding: FragmentPhotoPageBinding
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPhotoPageBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        binding.apply {
+            progressBar.max = 100
+
+            webView.apply {
+                settings.javaScriptEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(args.photoPageUri.toString())
+
+                webChromeClient = object : WebChromeClient() {
+                    override fun onProgressChanged(
+                        webView: WebView,
+                        newProgress: Int
+                    ) {
+                        if (newProgress == 100) {
+                            progressBar.visibility = View.GONE
+                        } else {
+                            progressBar.visibility = View.VISIBLE
+                            progressBar.progress = newProgress
+                        }
+                    }
+
+                    override fun onReceivedTitle(
+                        view: WebView?,
+                        title: String?
+                    ) {
+                        val parent = requireActivity() as AppCompatActivity
+                        parent.supportActionBar?.subtitle = title
+                    }
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        webView = binding.webView
+
+        val callback = object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    isEnabled = false
+                    requireActivity().onBackPressed()
+                }
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
+
+
+}
